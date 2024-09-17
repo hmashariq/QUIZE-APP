e64c61ea09b2882a0bd73d54ea2baaf3ccc66fb9
hmashariq <hmashariq>
2024-09-17 14:52:44 +0500

    quize app

---
+++ app.js
@@ -0,0 +1,44 @@
+function check(){
+
+      score = 0
+
+    var right_awnser_1 = document.getElementById('q1-a3')
+    var q1_awnser_1 = document.getElementById('q1-a1')
+    var q1_awnser_1 = document.getElementById('q1-a2')
+  if (right_awnser_1.checked == true) {
+    score++
+    alert('q1 answer is correct')
+  }
+  else{
+    alert('q1 answer is wrong')
+}  
+
+var right_awnser_2 = document.getElementById("q2-a2")
+var q2_answer_2 = document.getElementById("q2-a2")
+var  q2_answer_2= document.getElementById("q2-a2")
+
+score++
+
+  if(right_awnser_2.checked == true){
+     alert('q2  answer is right')
+  }
+  else{
+    alert('q2  answer is wrong');
+  }
+
+
+  var right_awnser_3 = document.getElementById("q3-a1")
+  var q3_answer_3 = document.getElementById("q3-a3")
+  var  q3_answer_3= document.getElementById("q3-a3")
+  
+  score++
+  
+    if(right_awnser_1.checked == true){
+       alert('q3  answer is right')
+    }
+    else{
+      alert('q3  answer is wrong');
+    }
+    }
+  
+
